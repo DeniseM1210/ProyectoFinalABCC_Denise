b5a1a96bfa92708221f8887173e7723f216019d5
unknown <denisemorales8764@gmail.com>
2021-05-31 17:26:21 -0500

    Se agregaron los getters y setters, constructores y el toString a la clase Empleado

--- src/modelo/Empleado.java
+++ src/modelo/Empleado.java
@@ -1,6 +1,58 @@
 package modelo;
 
 public class Empleado {
+	private String RFCEmpleado;
+	private String Nombre;
+	private String Apellidos;
+	private int idPuesto;
+	
+	public Empleado() {}
+	
+	public Empleado(String rFCEmpleado, String nombre, String apellidos, int idPuesto) {
+		super();
+		RFCEmpleado = rFCEmpleado;
+		Nombre = nombre;
+		Apellidos = apellidos;
+		this.idPuesto = idPuesto;
+	}
+
+	public String getRFCEmpleado() {
+		return RFCEmpleado;
+	}
+
+	public void setRFCEmpleado(String rFCEmpleado) {
+		RFCEmpleado = rFCEmpleado;
+	}
+
+	public String getNombre() {
+		return Nombre;
+	}
+
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return Apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		Apellidos = apellidos;
+	}
+
+	public int getIdPuesto() {
+		return idPuesto;
+	}
+
+	public void setIdPuesto(int idPuesto) {
+		this.idPuesto = idPuesto;
+	}
+
+	@Override
+	public String toString() {
+		return "Empleado [RFCEmpleado=" + RFCEmpleado + ", Nombre=" + Nombre + ", Apellidos=" + Apellidos
+				+ ", idPuesto=" + idPuesto + "]";
+	}
 	
 
 }
