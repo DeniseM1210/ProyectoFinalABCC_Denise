bc1110d4859276f70037cf62c8eb6cef57b3382c
DeniseM1210 <71150096+DeniseM1210@users.noreply.github.com>
2021-06-01 00:44:46 -0500

    Se comenzo con la interfaz general del usuario, donde se veran las ventanas de altas, bajas, consultas y cambios

--- src/vista/VentanaInicio.java
+++ src/vista/VentanaInicio.java
@@ -7,13 +7,17 @@ import java.awt.event.ActionListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JInternalFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPasswordField;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 
@@ -37,20 +41,53 @@ class Interfaz extends JFrame{
 	JMenuBar menuBar = new JMenuBar();
 	JMenu cliente = new JMenu("Cliente");
 	JMenuItem altasCli, bajasCli, cambiosCli, consultasCli;
+	JInternalFrame if_altasCli, if_bajasCli, if_cambiosCli, if_consultasCli;
+	
 	JMenu compraProducto = new JMenu("Compra de Producto");
 	JMenuItem altasCom, bajasCom, cambiosCom, consultasCom;
+	JInternalFrame if_altasCom, if_bajasCom, if_cambiosCom, if_consultasCom;
+	
 	JMenu empleado = new JMenu("Empleado");
-	JMenuItem altasEmp, bajasEmp, 
+	JMenuItem altasEmp, bajasEmp, cambiosEmp, consultasEmp;
+	JInternalFrame if_altasEmp, if_bajasEmp, if_cambiosEmp, if_consultasEmp;
+	
 	JMenu producto = new JMenu("Producto");
+	JMenuItem altasProd, bajasProd, cambiosProd, consultasProd;
+	JInternalFrame if_altasProd, if_bajasProd, if_cambiosProd, if_consultasProd;
+	
 	JMenu proveedor = new JMenu("Proveedor");
+	JMenuItem altasProv, bajasProv, cambiosProv, consultasProv;
+	JInternalFrame if_altasProv, if_bajasProv, if_cambiosProv, if_consultasProv; 
+	
 	JMenu puestosTrabajo = new JMenu("Puestos de Trabajo");
+	JMenuItem altasPt, bajasPt, cambiosPt, consultasPt;
+	JInternalFrame if_altasPt, if_bajasPt, if_cambiosPt, if_consultasPt;
+	
+	ImageIcon iconoAgregar = new ImageIcon("./archivos/anadir.png");
+	ImageIcon iconoBorrar = new ImageIcon("./archivos/eliminar.png");
+	ImageIcon iconoLimpiar = new ImageIcon("./archivos/limpio.png");
+	ImageIcon iconoEliminar = new ImageIcon("./archivos/eliminar.png");
+	ImageIcon iconoBuscar = new ImageIcon("./archivos/lupa.png");
+	ImageIcon iconoGuardar = new ImageIcon("./archivos/guardar.png");
+	
+	JButton btnAgregar, btnBorrar, btnLimpiar, btnEliminar, btnBuscar, btnGuardar;
+	JTextField cajaNombre, cajaidCliente, cajaCorreoE, cajaNumTel, cajaDireccion,
+	cajaClaveProducto, cajaFecha, cajaNombreProd, cajaColor, cajaIdEmp, cajaRFC,
+	cajaApellido, cajaIdPuesto, cajaDescripcion, cajaPrecio, cajaIdProveedor, cajaProductoProvee,
+	cajaNombrePuesto;
+	
+	JComboBox comboCantidad, comboDisponibilidad;
+	
+	JTable tablaCliente, tablaCompraProducto, tablaEmpleado, tablaProducto,
+	tablaProveedor, tablaPuestosTrabajo;
+	JScrollPane sp = new JScrollPane();
 	
 	
 	public Interfaz() {
 		getContentPane().setLayout(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setSize(584, 490);
-		setTitle("Talabarteria");
+		setTitle("Talabarteria Estrella");
 		setLocationRelativeTo(null);
 		
 		
