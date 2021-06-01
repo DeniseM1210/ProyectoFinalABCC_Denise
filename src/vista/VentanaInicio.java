1b363308e67c9bb793a1ef267f83ac34e9a31540
DeniseM1210 <71150096+DeniseM1210@users.noreply.github.com>
2021-06-01 00:00:56 -0500

    Se termino la interfaz de login y se agrego verificacion de usuario y contraseña

--- src/vista/VentanaInicio.java
+++ src/vista/VentanaInicio.java
@@ -1,10 +1,18 @@
 package vista;
 
+import java.awt.Color;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
@@ -28,11 +36,15 @@ class Interfaz extends JFrame{
 	
 	JMenuBar menuBar = new JMenuBar();
 	JMenu cliente = new JMenu("Cliente");
+	JMenuItem altasCli, bajasCli, cambiosCli, consultasCli;
 	JMenu compraProducto = new JMenu("Compra de Producto");
+	JMenuItem altasCom, bajasCom, cambiosCom, consultasCom;
 	JMenu empleado = new JMenu("Empleado");
+	JMenuItem altasEmp, bajasEmp, 
 	JMenu producto = new JMenu("Producto");
 	JMenu proveedor = new JMenu("Proveedor");
 	JMenu puestosTrabajo = new JMenu("Puestos de Trabajo");
+	
 	
 	public Interfaz() {
 		getContentPane().setLayout(null);
@@ -48,11 +60,11 @@ class Interfaz extends JFrame{
 	
 }
 
-class Login extends JFrame{
-	JTextField txtUsuario = new JTextField();
-	JPasswordField txtContra = new JPasswordField();
+class Login extends JFrame implements ActionListener{
+	JTextField cajaUsuario = new JTextField();
+	JPasswordField cajaContra = new JPasswordField();
 	
-	JButton ingresar = new JButton("Ingresar");
+	JButton btnIngresar = new JButton("Ingresar");
 	ImageIcon iconoUsuario = new ImageIcon("./archivos/usuario.png");
 	
 	public Login() {
@@ -62,9 +74,50 @@ class Login extends JFrame{
 		setLocationRelativeTo(null);
 		setTitle("Login");
 		
+		JLabel lblImg = new JLabel();
+		lblImg.setBounds(70, 35, 150, 150);
+		lblImg.setIcon(new ImageIcon(iconoUsuario.getImage().getScaledInstance(150, 150, Image.SCALE_SMOOTH)));
+		add(lblImg);
 		
+		JLabel lblUsuario = new JLabel("Usuario:");
+		lblUsuario.setBounds(40, 200, 200, 20);
+		add(lblUsuario);
+		cajaUsuario.setBounds(40, 230, 200, 20);
+		add(cajaUsuario);
+		
+		JLabel lblContra = new JLabel("Contraseña: ");
+		lblContra.setBounds(40, 260, 200, 20);
+		add(lblContra);
+		cajaContra.setBounds(40, 290, 200, 20);
+		add(cajaContra);
+		
+		btnIngresar.setBounds(90, 320, 100, 20);
+		btnIngresar.setBackground(Color.getHSBColor(285, 20, 100));
+		btnIngresar.setForeground(Color.BLACK);
+		btnIngresar.addActionListener(this);
+		add(btnIngresar);
 		
 		setVisible(true);
+	}
+
+	@SuppressWarnings("deprecation")
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(cajaUsuario.getText().equals("") || cajaContra.getText().equals("")) {
+			JOptionPane.showMessageDialog(null, "ERROR! Faltan datos");
+		}else if(cajaUsuario.getText().equals("Usuario") || cajaContra.getText().equals("12345")) {
+			SwingUtilities.invokeLater(new Runnable() {
+				
+				@Override
+				public void run() {
+					new Interfaz();
+				}
+			});
+			setVisible(false);
+		}else {
+			JOptionPane.showMessageDialog(null, "Datos incorrectos");
+		}
+		
 	}
 }
 
