6114eeb230ab897553b030ed425e4ce3143ec009
unknown <denisemorales8764@gmail.com>
2021-05-31 19:57:32 -0500

    Se agrego una clase llamada ConsultaProductos dentro de ProductoDAO para poder llevar a cabo la accion de buscar

--- src/controlador/ProductoDAO.java
+++ src/controlador/ProductoDAO.java
@@ -1,7 +1,59 @@
 package controlador;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 import conexionBD.ConexionBD;
 import modelo.Producto;
+
+class ConsultaProductos implements Runnable{
+	ArrayList<Producto> listaProductos = new ArrayList<Producto>();
+	String filtro;
+	
+	public ConsultaProductos(String filtro) {
+		this.filtro = filtro;
+	}
+
+	@Override
+	public void run() {
+		ResultSet rs;
+		
+		rs = ConexionBD.ejecutarConsulta(filtro);
+		
+		try {
+			if(rs.next()) {
+				do {
+					listaProductos.add(new Producto(rs.getString(1),
+							rs.getString(2),
+							rs.getInt(3), 
+							rs.getString(4)));
+				}while(rs.next());
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	public ArrayList<Producto> getListaProductos() {
+		return listaProductos;
+	}
+
+	public void setListaProductos(ArrayList<Producto> listaProductos) {
+		this.listaProductos = listaProductos;
+	}
+
+	public String getFiltro() {
+		return filtro;
+	}
+
+	public void setFiltro(String filtro) {
+		this.filtro = filtro;
+	}
+	
+	
+}
 
 public class ProductoDAO{
 	
@@ -30,5 +82,7 @@ public class ProductoDAO{
 		resultado = ConexionBD.eliminarRegistro(sql);
 		return resultado;
 	}
+	
+	
 
 }
